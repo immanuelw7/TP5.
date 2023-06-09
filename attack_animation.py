8fdd12407c942eea80085a4366f67335765e1042
immanuelw7 <112488057+immanuelw7@users.noreply.github.com>
2023-06-08 22:12:26 -0400

    Update attack_animation.py

--- attack_animation.py
+++ attack_animation.py
@@ -10,42 +10,47 @@ class AttaqueType(Enum):
 
 
 class AttackAnimation(arcade.Sprite):
+    """ This class manages the animation for our character.
+    It contains both the animation and the logic behind it.
+    """
     ATTACK_SCALE = 0.50
     ANIMATION_SPEED = 5.0
 
-    def __init__(self, atack_type):
+    def __init__(self, attack_type):
         super().__init__()
-        self.an_update_time = 1.0 / AttackAnimation.ANIMATION_SPEED
+        self.animation_update_time = 1.0 / AttackAnimation.ANIMATION_SPEED
         self.time_since_last_swap = 0.0
-        self.atack_type = atack_type
-        if self.atack_type == AttaqueType.ROCK:
-            self.texture = [
+        self.attack_type = attack_type
+        if self.attack_type == AttaqueType.ROCK:
+            self.textures = [
                 arcade.load_texture("assets/srock.png"),
                 arcade.load_texture("assets/srock-attack.png"),
             ]
-        elif self.atack_type == AttaqueType.PAPER:
-            self.texture = [
+        elif self.attack_type == AttaqueType.PAPER:
+            self.textures = [
                 arcade.load_texture("assets/spaper.png"),
                 arcade.load_texture("assets/spaper-attack.png"),
             ]
         else:
-            self.texture = [
+            self.textures = [
                 arcade.load_texture("assets/scissors.png"),
                 arcade.load_texture("assets/scissors-close.png"),
             ]
 
         self.scale = self.ATTACK_SCALE
-        self.current_texxt_lol = 0
-        self.set_texture(self.current_texxt_lol)
+        self.current_texture = 0
+        self.set_texture(self.current_texture)
 
     def on_update(self, delta_time: float = 1 / 60):
+        """ Update the animation.
+        """
         # Update the animation.
         self.time_since_last_swap += delta_time
-        if self.time_since_last_swap > self.an_update_time:
-            self.current_texxt_lol += 1
-            if self.current_texxt_lol < len(self.texture):
-                self.set_texture(self.current_texxt_lol)
+        if self.time_since_last_swap > self.animation_update_time:
+            self.current_texture += 1
+            if self.current_texture < len(self.textures):
+                self.set_texture(self.current_texture)
             else:
-                self.current_texxt_lol = 0
-                self.set_texture(self.current_texxt_lol)
+                self.current_texture = 0
+                self.set_texture(self.current_texture)
             self.time_since_last_swap = 0.0
